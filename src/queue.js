261d15aeb88e2a22411c496d3797ce6bc060e9d9
innashen <101499971+innashen@users.noreply.github.com>
2022-10-17 23:14:00 +0300

    feat: done half

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+      this.head = null;
+      this.end = null;
+      this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+      return this.head
+  }
+
+  enqueue(value) {
+    
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      let head = this.head.value;
+      this.head = this.head.next;
+      this.length--;
+      return head;
   }
 }
 
